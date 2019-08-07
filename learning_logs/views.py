4b4a138e35c3e4e8d5515bcf089320a70fafa87c
alecbert <alec.bertossa@gmail.com>
2019-08-07 18:03:11 -0400

    Let people make a new entry

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Topic
-from .forms import TopicForm
+from .forms import TopicForm, EntryForm
 
 def index(request):
     """The home page for Learning Log"""
@@ -34,3 +34,24 @@ def new_topic(request):
     # Display a blank or invalid form
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    """Add a new entry for a particular topic."""
+    # Still not a fan of this objects.get stuff. It feels like I'm not seeing enough and it "just works"
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        form = EntryForm()
+    else:
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            # What is commit=false? A: don't save to db yet
+            new_entry = form.save(commit=False)
+            # Not sure why the form.save and then new_entry.save? Is that the only way to get the entry object out
+            new_entry.topic = topic
+            new_entry.save()
+            # Redirecting within the view
+            return redirect('learning_logs:topic', topic_id=topic_id)
+
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
